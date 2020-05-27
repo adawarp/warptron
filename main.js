1519d12d824fc7fb0c853a3d865b444398a04dfc
azamiya <t.adaniya@adawarp.com>
2020-05-27 19:52:10 +0900

    feat: enable to connect sora, mqtt via usb json data

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-const {exec} = require('child_process');
-
-exec('sh momo.sh', (err, stdout, stderr) => {
-  if (err) { console.log(err); }
-  console.log(stdout);
-});
-
 const mqtt = require('mqtt')
 const client  = mqtt.connect('mqtt://160.16.238.254')
 
-const userName = 'roid2'
+const fs = require('fs');
+const rawData = fs.readFileSync('warp-key.json');
+const key = JSON.parse(rawData);
+
+const userName = key.roidId;
+console.log(userName);
+
+const {exec} = require('child_process');
+const execMomoCommand = `./momo --log-level 2 --no-audio-device sora wss://devwarp.work/signaling ${key.roidId} --auto --role sendrecv --multistream`;
+
+exec(execMomoCommand, (err, stdout, stderr) => {
+  if (err) { console.log(err); }
+  console.log(stdout);
+});
 
 const ARDUINO_PRO_MICRO_VENDOR_ID = '2341';
 const ARDUINO_PRO_MICRO_PRODUCT_ID = '8036';
@@ -28,6 +34,7 @@ client.on('connect', function () {
   })
 })
 
+
 client.on('message', function (topic, message) {
   if(topic === `${userName}-restart-momo`) {
     exec('killall momo', (err, stdout, stderr) => {
@@ -36,7 +43,7 @@ client.on('message', function (topic, message) {
     });
 
     setTimeout(() => {
-      exec('sh momo.sh', (err, stdout, stderr) => {
+      exec(execMomoCommand, (err, stdout, stderr) => {
         if (err) { console.log(err); }
         console.log(stdout);
       });
